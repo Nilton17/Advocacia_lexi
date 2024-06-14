07e6ed3a423d7141e775bacce781f67b6018da39
Nilton17 <niltonmario290@gmail.com>
2024-06-14 05:59:54 -0300

    subir os arquivos do meu sit

---
+++ exame_web_2.sql
@@ -0,0 +1,27 @@
+
+
+CREATE DATABASE IF NOT EXISTS exame_web_2;
+
+
+USE exame_web_2;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(110) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    texto VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO usuarios (nome, email, telefone, texto) 
+VALUES ('Nilton', 'nilton@gmail.com', '123456789', 'Ola Lexi & JurisPro');
+
+
+SELECT * FROM usuarios;
+
+-- Atualizar os detalhes de um usuário
+UPDATE usuarios SET telefone = '987654321' WHERE id = 1;
+
+-- Deletar um usuário
+DELETE FROM usuarios WHERE id = 1;
